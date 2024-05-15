86a5c6e1821177913e5a60800ad856c1f65172d7
Colin Silvers <silversc1998@gmail.com>
2024-05-15 15:27:55 -0400

    websocket initialization, server up

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const { Server } = require("socket.io");
+const { v4: uuidV4} = require('uuid');
+const http = require('http');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+const server = http.createServer(app);
+
+const port = process.env.PORT || 8080;
+
+const io = new Server(server, {
+  cors: '*', //this allows connection from any origin
+});
+
+//io connection with WebSocket
+io.on('connection', (socket) => {
+  // socket refers to the client socket that just got connected.
+  // each socket is assigned an id
+  console.log(socket.id, 'connected');
+});
+
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
+});
