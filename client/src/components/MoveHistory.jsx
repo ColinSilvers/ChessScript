a81968fb2184287fd71f772d972830d70bcb34de
Colin Silvers <silversc1998@gmail.com>
2024-06-11 13:50:43 -0400

    progress on moves history container

--- client/src/components/MoveHistory.jsx
+++ client/src/components/MoveHistory.jsx
@@ -1,16 +1,48 @@
-import { Chessboard } from "react-chessboard";
-import { Chess } from "chess.js";
-import * as react from 'react';
-import { blue } from "@mui/material/colors";
-
 
 const MovesHistory = ({moves}) => {
 
+  const movesCopy = [...moves];
+
+  const colOne = getWhiteMoves();
+  const colTwo = getBlackMoves();
+
+  function getWhiteMoves() {
+    let counter = 1;
+    const whiteMoves = [];
+    for(let i = 0; i < movesCopy.length; i++) {
+      if(i % 2 === 0) {
+        whiteMoves.push(`${counter++}. ` + movesCopy[i]);
+      }
+    }
+    return whiteMoves.join('\r\n');
+  }
+
+  function getBlackMoves() {
+    let counter = 1;
+    const blackMoves = [];
+    for(let i = 0; i < movesCopy.length; i++) {
+      if(i % 2 !== 0) {
+        blackMoves.push(`${counter++}. ` + movesCopy[i]);
+      }
+    }
+    return blackMoves.join('\r\n');
+  }
 
   return (
-    <div className="moves-container">
-      {moves}
+    <div class='some-page-wrapper'>
+  <div class='row'>
+    <div class='column'>
+      <div class='white-column'>
+        {colOne}
+      </div>
     </div>
+    <div class='column'>
+      <div class='black-column'>
+        {colTwo}
+      </div>
+    </div>
+  </div>
+</div>
   )
 
 }
